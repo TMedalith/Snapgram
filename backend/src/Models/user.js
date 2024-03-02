f959648192f9dec76d407ab3da825bf714b4ddb2
TMedalith <u20211f955@upc.edu.pe>
2024-03-02 15:34:11 -0500

    feat: Add user model

---
+++ backend/src/Models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/1053/1053244.png",
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+
+});
+
+module.exports = mongoose.model("User", userSchema);
